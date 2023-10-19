10a218515833b5e1c5286552a8d8868076125a3b
Zehui Zheng <work996mail@gmail.com>
2023-10-19 22:20:14 +0800

    WIP

---
+++ front-end/network.py
@@ -0,0 +1,42 @@
+import socket
+from structure import *
+import sys
+
+class Network:
+    server_ip = ""
+    server_port = 5555
+
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect((Network.server_ip, Network.server_port))
+
+    def get_data(self):
+        data = self.client.recv(1024).decode()
+        if not data:
+            print('\nDisconnected from server\n')
+            sys.exit()
+        return data
+    
+    # get data from Player struct
+    
+    def get_role(self) -> str:
+        self.server.send("Player\n")
+        return Role(self.get_data()).name
+    
+    def is_alve(self) -> bool:
+        self.server.send("Player\n")
+        return Role(self.get_data()).is_alive
+    
+    # get data from GameState struct
+
+    def is_finished(self) -> bool:
+        self.server.send("GameState\n")
+        return GameState(self.get_data()).is_finished
+    
+    def is_day(self) -> bool:
+        self.server.send("GameState\n")
+        return GameState(self.get_data()).is_day
+    
+    def get_round(self) -> int:
+        self.server.send("GameState\n")
+        return GameState(self.get_data()).round
