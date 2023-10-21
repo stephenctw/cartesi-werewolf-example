c4f03e79b9cc0224411bda03eed15012f40f15a6
Zehui Zheng <work996mail@gmail.com>
2023-10-21 20:34:36 +0800

    WIIP

--- front-end/network.py
+++ front-end/network.py
@@ -10,6 +10,19 @@ class Network:
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.connect((Network.server_ip, Network.server_port))
 
+    def join_game(self, id, pk):
+        self.server.send("Join. ID: ",id, "PK: ", pk)
+
+    # send message
+    def send(self, id, message):
+        # send by socket
+
+    def send_on_chain(self, message):
+
+    def broadcast_all(self, message):
+
+    # get data routine
+
     def get_data(self):
         data = self.client.recv(1024).decode()
         if not data:
@@ -17,11 +30,19 @@ class Network:
             sys.exit()
         return data
     
-    # get data from Player struct
+    def get_raw_data(self):
+        raw_data = self.client.recv(1024)
+        if not raw_data:
+            print('\nDisconnected from server\n')
+            sys.exit()
+        return raw_data
     
-    def get_role(self) -> str:
-        self.server.send("Player\n")
-        return Role(self.get_data()).name
+    def get_on_chain(self):
+
+    def get_players(self): # Game._players
+        return self.get_data()
+    
+    # get data from Player struct
     
     def is_alve(self) -> bool:
         self.server.send("Player\n")
@@ -40,3 +61,9 @@ class Network:
     def get_round(self) -> int:
         self.server.send("GameState\n")
         return GameState(self.get_data()).round
+    
+    def should_revote(self)
+
+    def get_mod_pk(self) 
+
+
